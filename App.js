a4e5f1be627301f1f28817b5fdf4211fbe477505
Rafael Bertelli <rafaelbertelli89@gmail.com>
2018-11-12 23:04:45 -0200

    display presentation

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Button from './src/components/Button'
+import Display from './src/components/Display'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -10,25 +11,33 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
-  render() {
+  state = {
+    displayValue: 0
+  }
+
+  render () {
     return (
       <View style={styles.container}>
+        <View style={styles.display}>
+          <Display value={this.state.displayValue} />
+        </View>
         <View style={styles.button}>
-          <Button label={1} />
-          <Button label={2} />
-          <Button label={3} />
-          <Button label={4} />
-          <Button label={5} />
-          <Button label={6} />
+          <Button label={'AC'} />
+          <Button label={'/'} />
           <Button label={7} />
           <Button label={8} />
           <Button label={9} />
+          <Button label={1} />
+          <Button label={6} />
+          <Button label={3} />
+          <Button label={2} />
+          <Button label={4} />
+          <Button label={5} />
+          <Button label={0} />
           <Button label={'-'} />
           <Button label={'+'} />
           <Button label={'*'} />
-          <Button label={'/'} />
           <Button label={'='} />
-          <Button label={'AC'} />
           <Button label={'.'} />
         </View>
       </View>
@@ -38,6 +47,9 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  display: {
     flex: 1
   },
   button: {
