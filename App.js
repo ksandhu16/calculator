dc3b5312977beb5770a0d23394b33b16932cc382
Rafael Bertelli <rafaelbertelli89@gmail.com>
2018-11-15 16:09:53 -0200

    added calculator logic and setted oritentation to be only portrait

--- App.js
+++ App.js
@@ -3,28 +3,73 @@ import { StyleSheet, View } from 'react-native'
 import Button from './src/components/Button'
 import Display from './src/components/Display'
 
-export default class App extends Component {
-  state = {
-    displayValue: 0
-  }
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
 
-  addDigit = d => {
-    this.setState({ displayValue: d })
+export default class App extends Component {
+  state = { ...initialState }
+
+  addDigit = n => {
+    if (n === '.' && this.state.displayValue.includes('.')) return
+
+    const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+
+    this.setState({
+      displayValue,
+      clearDisplay: false
+    })
+
+    if (n !== '.') {
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+
+      this.setState({ values })
+    }
   }
 
   clearMemory = () => {
-    this.setState({ displayValue: 0 })
+    this.setState({ ...initialState })
   }
 
-  setOperation = o => {
-    console.log(o)
+  setOperation = operation => {
+    if (this.state.current === 0) {
+      this.setState({
+        operation,
+        current: 1,
+        clearDisplay: true
+      })
+    } else {
+      const equals = operation === '='
+      const values = [...this.state.values]
+
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+      } catch (e) {
+        values[0] = this.state.values[0]
+      }
+
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        // clearDisplay: !equals,
+        clearDisplay: true,
+        values
+      })
+    }
   }
   
-  componentDidMount = () =>
-  console.log('montou')
-
   render () {
-    console.log('renderizou')
     return (
       <View style={styles.container}>
         <View style={styles.display}>
